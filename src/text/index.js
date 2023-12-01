1c54473840b58cb08c76678d7ada6e251f4f35a6
htnamus <tangiralasumanth@gmail.com>
2023-12-01 14:51:54 -0500

    Fixed issue with displaying PDF

--- src/text/index.js
+++ src/text/index.js
@@ -1,11 +1,13 @@
 import rawText from './text.json';
 
 const mediaURL = 'https://raw.githubusercontent.com/sumanth-tangirala/personal-webpage/main/media/';
-const docsURL = 'https://nbviewer.org/github/sumanth-tangirala/personal-webpage/blob/main/media/'
+const docsURL = `https://docs.google.com/viewer?url=${mediaURL}`;
 
 const updatedText = JSON.stringify(rawText)
     .replaceAll("%MEDIA_URL%", mediaURL)
-    .replaceAll("%DOCS_URL%/", docsURL);
+    .replaceAll("%DOCS_URL%", docsURL);
+
+console.log()
 
 
 export default JSON.parse(updatedText);
