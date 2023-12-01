1d9b95df8efb7372570a915cc92157dbbff33124
htnamus <tangiralasumanth@gmail.com>
2023-12-01 15:26:02 -0500

    Fixed incorrect media URL issue

--- src/text/index.js
+++ src/text/index.js
@@ -1,6 +1,6 @@
 import rawText from './text.json';
 
-const mediaURL = 'https://raw.githubusercontent.com/sumanth-tangirala/personal-webpage/main/media/';
+const mediaURL = 'https://raw.githubusercontent.com/sumanth-tangirala/personal-webpage/main/media';
 const docsURL = `https://docs.google.com/viewer?url=${mediaURL}`;
 
 const updatedText = JSON.stringify(rawText)
