8835b2aabe47351300bd6a34c8497917111a05c6
htnamus <tangiralasumanth@gmail.com>
2024-08-12 12:29:54 +0530

    Added the State of Motion Generation to the website

--- src/text/index.js
+++ src/text/index.js
@@ -2,10 +2,12 @@ import rawText from "./text.json";
 
 const mediaURL =
   "https://raw.githubusercontent.com/sumanth-tangirala/personal-webpage/main/media";
-const docsURL = `https://docs.google.com/viewer?url=${mediaURL}`;
+const docsURL = `https://docs.google.com/viewer?url=${mediaURL}/docs`;
+const imgURL = `${mediaURL}/img`;
 
 const updatedText = JSON.stringify(rawText)
   .replaceAll("%MEDIA_URL%", mediaURL)
+  .replaceAll("%IMG_URL%", imgURL)
   .replaceAll("%DOCS_URL%", docsURL);
 
 export default JSON.parse(updatedText);
