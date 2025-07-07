60d631e0949c0986496fb81580c583cfe5a64144
sumanth-tangirala <tangiralasumanth@gmail.com>
2025-07-06 18:01:35 -0700

    Updated the layout of the about section and background colors of the cards

--- src/Components/AppBody/Components/About/About.js
+++ src/Components/AppBody/Components/About/About.js
@@ -9,41 +9,43 @@ import _map from "lodash/map";
 function About({ className, sectionRef }) {
   return (
     <div className={cx(styles.container, className)} ref={sectionRef}>
-      {mobileDesktopSwitcher({
-        mobile: (
-          <img
-            src={text.aboutMobilePicturePath}
-            className={styles.image}
-            alt=""
-          />
-        ),
-        desktop: (
-          <img src={text.aboutPicturePath} className={styles.image} alt="" />
-        ),
-      })}
+      <div className={styles.topSection}>
+        {mobileDesktopSwitcher({
+          mobile: (
+            <img
+              src={text.aboutMobilePicturePath}
+              className={styles.image}
+              alt=""
+            />
+          ),
+          desktop: (
+            <img src={text.aboutPicturePath} className={styles.image} alt="" />
+          ),
+        })}
 
-      <div className={styles.text}>
-        {/* <span className={styles.name}>{parse(text.name)}</span> */}
-        <div className={styles.qualifications}>
-          {_map(text.qualifications, (qual, idx) => (
-            <div key={idx}>{parse(qual)}</div>
-          ))}
+        <div className={styles.text}>
+          {/* <span className={styles.name}>{parse(text.name)}</span> */}
+          <div className={styles.qualifications}>
+            {_map(text.qualifications, (qual, idx) => (
+              <div key={idx}>{parse(qual)}</div>
+            ))}
+          </div>
+          <div className={styles.aboutParagraphs}>
+            {_map(text.about, (paragraph, idx) => (
+              <span className={styles.aboutParagraphItem} key={idx}>
+                {parse(paragraph)}
+              </span>
+            ))}
+          </div>
         </div>
-        <div className={styles.aboutParagraphs}>
-          {_map(text.about, (paragraph, idx) => (
-            <span className={styles.aboutParagraphItem} key={idx}>
-              {parse(paragraph)}
-            </span>
-          ))}
-        </div>
-        <div className={styles.qualificationDetails}>
-          {_map(text.qualificationDetails, (qualificationDetail, idx) => (
-            <div className={styles.qualificationDetailItem} key={idx}>
-              <div className={styles.degree}>{parse(qualificationDetail.degree)}</div>
-              <div className={styles.university}>{parse(qualificationDetail.university)}</div>
-            </div>
-          ))}
-        </div>
+      </div>
+      <div className={styles.educationDetails}>
+        {_map(text.educationDetails, (educationDetail, idx) => (
+          <div className={styles.educationDetailItem} key={idx}>
+            <div className={styles.degree}>{parse(educationDetail.degree)}</div>
+            <div className={styles.university}>{parse(educationDetail.university)}</div>
+          </div>
+        ))}
       </div>
     </div>
   );
